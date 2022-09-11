5985b29612277ad4f056f297fa622814b32a98ec
TT-Tutorials <113271370+TT-Tutorials@users.noreply.github.com>
2022-09-11 20:28:01 +1000

    Update common.py

--- utilities/Settings/common.py
+++ utilities/Settings/common.py
@@ -29,7 +29,7 @@ import requests, os, sys, re, time, random, os.path, string, subprocess, random,
 
 #########################################
 
-THIS_VERSION = "1.5.0"
+THIS_VERSION = "2.5.0"
 
 w = Fore.WHITE
 b = Fore.BLACK
